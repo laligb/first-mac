ea6b37b35fb4005871f91d79ab2fb65de6f9f5a2
Lali Bibilashvili <lalibibilashvili@gmail.com>
2022-04-22 14:44:19 +0200

    layout and routeshave to be fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'user/new'
   root 'static_pages#home'
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
   get 'static_pages/contact'
+  get '/signup', to: 'user#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
