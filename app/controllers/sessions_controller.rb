faa8fc6f6a7451273b2d579a1a1d663b7781440d
Lali Bibilashvili <lalibibilashvili@gmail.com>
2022-04-26 15:12:48 +0200

    chapter progress

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # Log the user in and redirect to the user's show page.
+      reset_session
+      log_in user
+    else
+      # Create an error message.
+      flash.now[:danger] = 'Invalid email/password combination' # Not quite right
+      render 'new'
+    end
+  end
+end
