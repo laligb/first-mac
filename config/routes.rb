f4b5656932658372501e86f01709b1c67b542cc2
Lali Bibilashvili <lalibibilashvili@gmail.com>
2022-04-26 10:31:08 +0200

    singup needs to be corrected

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   get 'user/new'
   root 'static_pages#home'
   get 'static_pages/home'
@@ -6,5 +7,6 @@ Rails.application.routes.draw do
   get 'static_pages/about'
   get 'static_pages/contact'
   get '/signup', to: 'user#new'
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
