a00d93f9194fae352ddde6b19aee3271a3d39559
Lali Bibilashvili <lalibibilashvili@gmail.com>
2022-05-15 22:49:16 +0200

    added webpack and yarn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,12 +7,15 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       # Log the user in and redirect to the user's show page.
       reset_session
-      #log_in user
+      log_in user
       redirect_to user
     else
       # Create an error message.
       flash.now[:danger] = 'Invalid email/password combination' # Not quite right
       render 'new'
     end
+  end
+
+  def destroy
   end
 end
