f9a37dbc1f520bae924fcea5abc5939d53313607
Lali Bibilashvili <lalibibilashvili@gmail.com>
2022-05-16 11:15:54 +0200

    advanced login but needs to be fix test

--- Gemfile
+++ Gemfile
@@ -66,3 +66,5 @@ gem "autoprefixer-rails", "10.2.5"
 gem "font-awesome-sass", "~> 5.6.1"
 gem "simple_form", github: 'heartcombo/simple_form'
 gem 'bootstrap-sass', '3.4.1'
+
+gem 'rails-controller-testing'
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,9 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user&.authenticate(params[:session][:password])
-      # Log the user in and redirect to the user's show page.
+    if user && user.authenticate(params[:session][:password])
       reset_session
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       log_in user
       redirect_to user
     else
@@ -17,7 +17,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
-    redirect_to root_ur
+    log_out if logged_in?
+    redirect_to root_url
   end
 end
