f654c710ed8650eb4991b7c38bc0512c4d4a2d32
Lali Bibilashvili <lalibibilashvili@gmail.com>
2022-05-19 14:14:13 +0200

    little comment in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    redirect_to root_url and return unless FILL_IN
+    redirect_to root_url # and return unless FILL_IN
   end
 
   def new
