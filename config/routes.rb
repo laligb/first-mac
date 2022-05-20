07fc9889e1ab870f20dbb1ad365644bc0e7dcb03
Lali Bibilashvili <lalibibilashvili@gmail.com>
2022-05-20 17:24:22 +0200

    Add password reset

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  # get 'password_resets/new'
+  # get 'password_resets/edit'
   root 'static_pages#home'
-  get 'static_pages/home'
+  # get 'static_pages/home'
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
@@ -10,5 +12,6 @@ Rails.application.routes.draw do
   delete '/logout', to: 'sessions#destroy'
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :create, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -26,16 +26,13 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     assigns(:users).each do |user|
       assert user.FILL_IN
       assert_select 'a[href=?]', user_path(user), text: user.name unless user == @admin
-      assert_select 'a[href=?]', user_path(user), text: 'delete' end
-     end
-     assert_difference 'User.count', -1 do
-       delete user_path(@non_admin)
-     end
-end
+      assert_select 'a[href=?]', user_path(user), text: 'delete'
+    end
 
     assert_difference 'User.count', -1 do
       delete user_path(@non_admin)
     end
+
   end
 
   test "index as non-admin" do
