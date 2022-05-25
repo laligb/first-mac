fb40d0c62af46a8cc17b0dcfd65dca97f971ad6b
Lali Bibilashvili <lalibibilashvili@gmail.com>
2022-05-25 14:57:54 +0200

    fixed user activation issue

--- app/models/user.rb
+++ app/models/user.rb
@@ -59,10 +59,11 @@ class User < ApplicationRecord
     update_attribute(:remember_digest, nil)
   end
 
-  # Activates an account.
-  # def activate
-  #   update_columns(activated: FILL_IN, activated_at: FILL_IN)
-  # end
+   # Activates an account.
+  def activate
+    update_attribute(:activated,    true)
+    update_attribute(:activated_at, Time.zone.now)
+  end
 
   # Sends activation email.
   def send_activation_email
